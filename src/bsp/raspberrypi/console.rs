c96fcfccd0e3facccab056ad7193559a9a524c1d
Bug Digger <marko@bugdigger.com>
2023-12-13 00:14:30 +0100

    setting up the os to work with harder interrupts; implementation with print needs to be changed

--- src/bsp/raspberrypi/console.rs
+++ src/bsp/raspberrypi/console.rs
@@ -1,7 +1,7 @@
 //! BSP console facilities.
 
 use crate::console;
-use core::fmt;
+use core::{alloc, fmt};
 
 //--------------------------------------------------------------------------------------------------
 // Private Definitions
@@ -21,21 +21,48 @@ struct QEMUOutput;
 /// See [`src/print.rs`].
 ///
 /// [`src/print.rs`]: ../../print/index.html
-impl fmt::Write for QEMUOutput {
-    fn write_str(&mut self, s: &str) -> fmt::Result {
+impl console::interface::Write for QEMUOutput {
+
+    fn write_char(&self, c: char) {
+        // Implement the method write_char to write a character to QEMUOutput
+        // You can reuse the code in fmt::Write's write_str method
+        unsafe {
+            core::ptr::write_volatile(0x3F20_1000 as *mut u8, c as u8);
+        }
+    }
+
+    fn write_str(&self, s: &str) {
         for c in s.chars() {
             unsafe {
                 core::ptr::write_volatile(0x3F20_1000 as *mut u8, c as u8);
             }
         }
+    }
+
+    fn write_fmt(&self, args: fmt::Arguments) -> fmt::Result {
+        // In here, you could use write_str from fmt::Write trait to implement the write_fmt method
+        // using the format args.
+        // self.write_str(&format!("{}", args));
 
         Ok(())
+    }
+
+    fn flush(&self) {
+        // Implement the method to flush the buffer if necessary
+        // It would be NOP for QEMUOutput, hence left empty
     }
 }
 
 //--------------------------------------------------------------------------------------------------
 // Public Code
 //--------------------------------------------------------------------------------------------------
+
+// pub fn format(args: fmt::Arguments) -> alloc::string::String {
+//     use alloc::string::ToString;
+//     let mut output = String::new();
+//     let _ = fmt::write(&mut output, args);
+//     output
+// }
 
 /// Return a reference to the console.
 pub fn console() -> impl console::interface::Write {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+#![allow(clippy::upper_case_acronyms)]
 #![feature(asm_const)]
+#![feature(const_option)]
 #![feature(format_args_nl)]
+#![feature(nonzero_min_max)]
 #![feature(panic_info_message)]
+#![feature(trait_alias)]
+#![feature(unchecked_math)]
 
 #![no_std]
 #![no_main] // tell the Rust compiler that we don’t want to use the normal entry point chain
@@ -9,6 +14,9 @@ mod bsp;
 mod console;
 mod cpu;
 mod print;
+mod exception;
+mod time;
+mod synchronization;
 
 use core::panic::PanicInfo;
 use core::ptr::{read_volatile, write_volatile};
@@ -18,10 +26,62 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-/// Kernel entry point
+/// Early init code.
+///
+/// # Safety
+///
+/// - Only a single core must be active and running this function.
+/// - The init calls in this function must appear in the correct order.
+unsafe fn kernel_init() -> ! {
+    // Initialize the BSP driver subsystem.
+    // if let Err(x) = bsp::driver::init() {
+    //     panic!("Error initializing BSP driver subsystem: {}", x);
+    // }
+
+    // Initialize all device drivers.
+    // driver::driver_manager().init_drivers();
+    // println! is usable from here on.
+
+    // Transition from unsafe to safe.
+    kernelMain()
+}
+
+/// The main function running after the early init.
 #[no_mangle]
 pub extern "C" fn kernelMain() -> ! {
-    println!("Hello from ArmRustOs!");
+    use console::console;
+    use core::time::Duration;
 
-    loop {}
+    info!(
+        "{} version {}",
+        env!("CARGO_PKG_NAME"),
+        env!("CARGO_PKG_VERSION")
+    );
+    // info!("Booting on: {}", bsp::board_name());
+
+    let (_, privilege_level) = exception::current_privilege_level();
+    info!("Current privilege level: {}", privilege_level);
+
+    info!("Exception handling state:");
+    exception::asynchronous::print_state();
+
+    info!(
+        "Architectural timer resolution: {} ns",
+        time::time_manager().resolution().as_nanos()
+    );
+
+    info!("Drivers loaded:");
+    // driver::driver_manager().enumerate();
+
+    info!("Timer test, spinning for 1 second");
+    time::time_manager().spin_for(Duration::from_secs(1));
+
+    info!("Echoing input now");
+
+    // Discard any spurious received characters before going into echo mode.
+    console().clear_rx();
+    loop {
+        let c = console().read_char();
+        console().write_char(c);
+    }
 }
