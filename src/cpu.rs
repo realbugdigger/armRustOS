8c79fee33e50457c3db68df824b8b3fd08179712
Bug Digger <marko@bugdigger.com>
2023-12-27 11:23:04 +0100

    cleaning up

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,4 +1,3 @@
-#[cfg(target_arch = "aarch64")]
 #[path = "aarch64/cpu.rs"]
 mod arch_cpu;
 
--- src/cpu/boot.rs
+++ src/cpu/boot.rs
@@ -1,5 +1,4 @@
 //! Boot code.
 
-#[cfg(target_arch = "aarch64")]
 #[path = "../aarch64/cpu/boot.rs"]
 mod arch_boot;
