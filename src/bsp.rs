901f82434e5b3afea630878f4bf17dac08895786
Bug Digger <marko@bugdigger.com>
2023-12-29 17:57:32 +0100

    adding vector table and default exception handler method

--- src/bsp.rs
+++ src/bsp.rs
@@ -1,6 +1,6 @@
 //! Conditional reexporting of Board Support Packages.
 
-pub(crate) mod device_driver;
+mod device_driver;
 
 #[cfg(any(feature = "bsp_rpi3", feature = "bsp_rpi4"))]
 mod raspberrypi;
